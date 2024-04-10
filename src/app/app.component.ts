e9b4ebb505a6d68445e8c4f10fadc4558710f19c
AnantDhotre <anantdhotre68@gmail.com>
2024-04-10 11:26:04 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { ListEmployeeComponent } from './list-employee/list-employee.component';
+import { FormsModule } from '@angular/forms';
+import { CreateEmployeeComponent } from './create-employee/create-employee.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,ListEmployeeComponent,FormsModule,RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'frontend_for_springboot';
+  title = 'Employee-Management-System';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
-import { Routes } from '@angular/router';
+import { RouterModule,  Routes } from '@angular/router';
+import { ListEmployeeComponent } from './list-employee/list-employee.component';
+import { CreateEmployeeComponent } from './create-employee/create-employee.component';
+import { NgModule } from '@angular/core';
+import { UpdateEmployeeComponent } from './update-employee/update-employee.component';
+import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
+export const routes: Routes = [
 
-export const routes: Routes = [];
+    {path:'employees', component:ListEmployeeComponent},
+    {path:'create-employee',component:CreateEmployeeComponent},
+    {path:'', redirectTo:'employees', pathMatch:'full'},
+    {path:'update-employee/:id',component:UpdateEmployeeComponent},
+    {path:'employee-details/:id',component:EmployeeDetailsComponent}
+    
+   
+
+];
